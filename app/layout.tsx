0df4a5718568153b16ea48844f61a4535dd21d1e
Mikołaj Mrózek <mikolaj.mrozek@imgarena.com>
2022-11-19 10:02:17 +0100

    setup without pocketbase

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+import '../src/styles/globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/components/**/*.{js,ts,jsx,tsx}',
+    './src/ui-base/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
