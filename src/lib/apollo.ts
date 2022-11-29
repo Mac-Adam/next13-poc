3c25c95f19000bc423cfeaa64d8bb9c8acd3acc0
Mac-Adam <80308649+Mac-Adam@users.noreply.github.com>
2022-11-29 20:21:14 +0100

    api link update

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
-  uri: 'http://localhost:3000/api/graphql',
+  uri: 'https://next13-poc-hosting-test-q6w4.vercel.app/api/graphql',
   cache: new InMemoryCache(),
 });
 
